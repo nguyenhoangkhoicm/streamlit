01d95aaacacea01299aa235e625a1c5071aa3f1e
nguyenhoangkhoicm <73278567+nguyenhoangkhoicm@users.noreply.github.com>
2022-11-20 19:38:22 +0700

    Update fruit.py

--- fruit.py
+++ fruit.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from keras import keras_preprocessing
+
 from PIL import Image
-from keras_preprocessing.image import load_img,img_to_array
+from keras import load_img,img_to_array
 import numpy as np
 from keras.models import load_model
 import requests
